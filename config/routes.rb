7c73f5f6c4889eb06344df6ef60f12142d875155
abarnes26 <abarnes26@yahoo.com>
2018-02-28 16:58:26 -0700

    added function for deleting venues, artists

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   resources "users", only: [:new, :create, :edit]
   resources "artists", only: [:index, :show, :new, :create]
   resources "shows", only: [:index]
-  resources "venues", only: [:index, :show, :new, :create]
+  resources "venues", only: [:index, :show, :new, :create, :destroy]
 
   namespace :user do
     resources "shows", only: [:index, :new, :create, :edit, :destroy]
