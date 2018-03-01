d5b88aaa1412974a0e42a709410e037d0c7f92c7
abarnes26 <abarnes26@yahoo.com>
2018-02-28 18:27:21 -0700

    added styling to artist index page

--- app/controllers/user/shows_controller.rb
+++ app/controllers/user/shows_controller.rb
@@ -1,11 +1,7 @@
 class User::ShowsController < ApplicationController
 
   def index
-    @venue_markers = Array.new
     @venue_markers = current_user.visited_venues(current_user)
-    # current_user.venues.each do |venue|
-    #   @venue_markers << [venue.name, venue.latitude, venue.longitude]
-    # end
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true
-  has_many :artist_users, dependent: :destroy
   has_many :shows
+  has_many :artist_users, dependent: :destroy
+  has_many :user_venues, dependent: :destroy
   has_many :venues, through: :user_venues
   has_many :artists, through: :artist_users
-  has_many :user_venues
   validates :password, presence: true, length: {minimum: 5, maximum: 120}, on: :create
   validates :password, length: {minimum: 5, maximum: 120}, on: :update, allow_blank: true
 
   enum role: ["user", "admin"]
-
-  def show_list(user, venue)
-    Show.where(user_id: user.id).where(venue: venue)
-  end
-
+  
   def visited_venues(user)
     venue_list(user).map { |venue| [venue.name, venue.latitude, venue.longitude] }
   end
